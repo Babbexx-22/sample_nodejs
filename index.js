3dad1c5621945b1d03338c519924eee7c9719901
Ubuntu <ubuntu@ip-172-31-91-100.ec2.internal>
2023-07-07 14:57:14 +0000

    app and its associated folders

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
